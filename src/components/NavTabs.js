d3f2fbfedfbd533d354fd93d82419daa55fa78a9
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-08-03 11:10:22 -0400

    navtabs styling to space elements out

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,22 +1,28 @@
 import React from "react";
+import "../App.css";
 
 const bgkColor = {
   backgroundColor: "#3eaee7",
+  height: "45px",
 };
 
 const tabs = {
   color: "white",
   width: "40px",
+
+  fontSize: "23px",
+};
+
+const bar = {
+  width: "60%",
+  margin: "auto",
 };
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <div>
-      <ul
-        style={bgkColor}
-        className="nav nav-tabs d-flex justify-content-around"
-      >
-        <li className="nav-item">
+    <div style={bgkColor}>
+      <ul style={bar} className="nav nav-tabs d-flex justify-content-around">
+        <li id="indTabs" className="nav-item">
           <a
             style={tabs}
             href="#aboutMe"
@@ -28,7 +34,7 @@ function NavTabs({ currentPage, handlePageChange }) {
             About Me
           </a>
         </li>
-        <li className="nav-item">
+        <li id="indTabs" className="nav-item">
           <a
             style={tabs}
             href="#portfolio"
@@ -41,7 +47,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           </a>
         </li>
 
-        <li className="nav-item">
+        <li id="indTabs" className="nav-item">
           <a
             style={tabs}
             href="#resume"
